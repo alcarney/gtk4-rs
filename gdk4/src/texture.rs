af6ece7ab7fe5152acac31c475f02193b81f9ce6
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-12-16 17:12:05 +0100

    gdk: add sanity checks for Texture::download

--- gdk4/src/texture.rs
+++ gdk4/src/texture.rs
@@ -1,5 +1,6 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
+use crate::prelude::*;
 use crate::Texture;
 use glib::translate::*;
 use glib::IsA;
@@ -14,6 +15,14 @@ pub trait TextureExtManual: 'static {
 impl<O: IsA<Texture>> TextureExtManual for O {
     fn download(&self, data: &mut [u8], stride: usize) {
         unsafe {
+            assert!(
+                stride >= 4,
+                "Stride for a CAIRO_FORMAT_ARGB32 should be >= 4"
+            );
+            assert!(
+                data.len() as i32 == 4 * (stride as i32) * self.as_ref().height(),
+                "The data is not big enough to download the texture"
+            );
             ffi::gdk_texture_download(self.as_ref().to_glib_none().0, data.as_mut_ptr(), stride);
         }
     }
